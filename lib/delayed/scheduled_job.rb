7ff920bee88573967cbdba713ae8b84adc6bc80b
Justin Louie <jlouie@narble.com>
2010-05-15 13:10:51 -0700

    Modified ScheduledJob to use Chronic the natural languge parser
    It depends upon chronic, but it still needs to 'require' it.

--- lib/delayed/scheduled_job.rb
+++ lib/delayed/scheduled_job.rb
@@ -4,7 +4,7 @@ module Delayed
 	  def self.included(base)
 	    base.extend(ClassMethods)
 	  end
-	  
+
 	  module ClassMethods
 	    def method_added(name)
 	      if name.to_s == "perform" && !@redefined
@@ -12,20 +12,26 @@ module Delayed
 	        alias_method_chain :perform, :schedule
 	      end
 	    end
-	    
+
 	    def schedule
 	      @schedule
 	    end
-	    
-	    def run_every(time)
+
+	    #Uses Chronic the natural language parser
+	    def run(time)
 	      @schedule = time
-	    end  
+	    end
+
+       #An attempt at using :run_at => "8 am", :run_every => 1.days
+       #ultimately it'll be like Time.parse("8 am").1.day.from_now <- Doesn't work, lol.
+#      def run(args)
+#        @schedule = args
+#      end
 	  end
-	  
+
     def schedule!
-      #Delayed::Job.enqueue self, 0, self.class.schedule.from_now if self.class.schedule
-      #nitsujri's patch to remove .from_now to be able to schedule any time
-      Delayed::Job.enqueue self, 0, self.class.schedule if self.class.schedule
+      #nitsujri's patch to remove .from_now to be able to schedule any time, relies on Chronic
+      Delayed::Job.enqueue self, 0, Chronic.parse(self.class.schedule) if self.class.schedule
     end
 
 	  def perform_with_schedule
@@ -34,3 +40,4 @@ module Delayed
 	  end
   end
 end
+
